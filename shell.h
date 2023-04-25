f2851dd06c645c32fa405a317c2d2b600cec9374
Maunducyrus <maunducyrus123@gmail.com>
2023-04-25 06:03:09 -0700

    get_information

--- shell.h
+++ shell.h
@@ -171,7 +171,7 @@ ssize_t get_input(info_t *);
 int _getline(info_t *, char **, size_t *);
 void sigintHandler(int);
 
-void clear_info(info_t *);
+void information(info_t *);
 void set_info(info_t *, char **);
 void free_info(info_t *, int);
 
